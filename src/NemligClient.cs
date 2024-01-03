187eee42342b6a02d58c51f306155bbe77862296
Kim Ipsen <kim.ipsen@outlook.dk>
2024-01-03 15:28:16 +0100

    Using oneof

--- src/NemligClient.cs
+++ src/NemligClient.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json;
 using System.Threading.Tasks;
 using Flurl.Http;
+using OneOf;
 
 namespace NemligSharp;
 
@@ -10,41 +11,46 @@ public class NemligClient : INemligClient
     private readonly IFlurlClient _flurlClient = new FlurlClient(NemligBaseUrl);
     private CookieJar _cookieJar = null;
 
-    public async Task<ILoginResponse> LoginAsync(string userName, string password)
+    public bool IsReady => _cookieJar != null;
+
+    public async Task<OneOf<ILoginResponse, ErrorResponse>> LoginAsync(string userName, string password)
     {
         var payload = new LoginPayload(userName, password);
         var response = await _flurlClient.Request("webapi", "login").WithCookies(out var jar).PostJsonAsync(payload).ConfigureAwait(false);
+
+        if (response.StatusCode == 404) return new NotFoundResponse();
+        if (response.StatusCode != 200) return new UnknownErrorResponse();
+
         var jsonString = await response.GetStringAsync().ConfigureAwait(false);
         var deserialized = JsonSerializer.Deserialize<LoginResponse>(jsonString);
 
         _cookieJar = jar;
 
-        return deserialized with { StatusCode = response.StatusCode };
+        return deserialized;
     }
 
-    public async Task<ICurrentUserResponse> GetCurrentUserAsync() => await DoFlurlGet<CurrentUserResponse, ICurrentUserResponse>(["webapi", "user", "GetCurrentUser"]).ConfigureAwait(false);
+    public async Task<OneOf<ICurrentUserResponse, ErrorResponse>> GetCurrentUserAsync() => await DoFlurlGetV2<CurrentUserResponse, ICurrentUserResponse>(["webapi", "user", "GetCurrentUser"]).ConfigureAwait(false);
 
-    public async Task<IOrderHistoryResponse> GetOrderHistoryAsync(int skip, int take)
-    {
-        return await DoFlurlGet<OrderHistoryResponse, IOrderHistoryResponse>(["webapi", "order", "GetBasicOrderHistory"], new { skip, take });
-    }
+    public async Task<OneOf<IOrderHistoryResponse, ErrorResponse>> GetOrderHistoryAsync(int skip, int take) => await DoFlurlGetV2<OrderHistoryResponse, IOrderHistoryResponse>(["webapi", "order", "GetBasicOrderHistory"], new { skip, take });
 
-    public async Task<IOrderResponse> GetOrderAsync(int orderId) => await DoFlurlGet<OrderResponse, IOrderResponse>(["webapi", "order", "GetOrderHistory"], new { orderNumber = orderId });
+    public async Task<OneOf<IOrderResponse, ErrorResponse>> GetOrderAsync(int orderId) => await DoFlurlGetV2<OrderResponse, IOrderResponse>(["webapi", "order", "GetOrderHistory"], new { orderNumber = orderId });
 
-    public async Task<IShoppingListsResponse> GetShoppingListsAsync(int skip, int take) => await DoFlurlGet<ShoppingListsResponse, IShoppingListsResponse>(["webapi", "ShoppingList", "GetShoppingLists"], new { skip, take });
+    public async Task<OneOf<IShoppingListsResponse, ErrorResponse>> GetShoppingListsAsync(int skip, int take) => await DoFlurlGetV2<ShoppingListsResponse, IShoppingListsResponse>(["webapi", "ShoppingList", "GetShoppingLists"], new { skip, take });
 
-    public async Task<IShoppingListResponse> GetShoppingListAsync(int shoppingListId) => await DoFlurlGet<ShoppingListResponse, IShoppingListResponse>(["webapi", "ShoppingList", "GetShoppingList"], new { listId = shoppingListId });
+    public async Task<OneOf<IShoppingListResponse, ErrorResponse>> GetShoppingListAsync(int shoppingListId) => await DoFlurlGetV2<ShoppingListResponse, IShoppingListResponse>(["webapi", "ShoppingList", "GetShoppingList"], new { listId = shoppingListId });
 
-    public async Task<ICurrentBasketResponse> GetCurrentBasketAsync() => await DoFlurlGet<CurrentBasketResponse, ICurrentBasketResponse>(["webapi", "basket", "GetBasket"]);
+    public async Task<OneOf<ICurrentBasketResponse, ErrorResponse>> GetCurrentBasketAsync() => await DoFlurlGetV2<CurrentBasketResponse, ICurrentBasketResponse>(["webapi", "basket", "GetBasket"]);
 
-    private async Task<TResponseInterface> DoFlurlGet<TResponseImplementation, TResponseInterface>(string[] pathSegments, object queryParameters = null)
+    private async Task<OneOf<TResponseInterface, ErrorResponse>> DoFlurlGetV2<TResponseImplementation, TResponseInterface>(string[] pathSegments, object queryParameters = null)
         where TResponseImplementation : Response, TResponseInterface
         where TResponseInterface : IResponse
     {
         var response = await _flurlClient.Request(pathSegments).SetQueryParams(queryParameters).WithCookies(_cookieJar).GetAsync().ConfigureAwait(false);
-        var jsonString = await response.GetStringAsync().ConfigureAwait(false);
-        var deserialized = JsonSerializer.Deserialize<TResponseImplementation>(jsonString);
 
-        return deserialized with { StatusCode = response.StatusCode };
+        if (response.StatusCode == 404) return new NotFoundResponse();
+        if (response.StatusCode != 200) return new UnknownErrorResponse();
+
+        var jsonString = await response.GetStringAsync().ConfigureAwait(false);
+        return JsonSerializer.Deserialize<TResponseImplementation>(jsonString);
     }
 }
