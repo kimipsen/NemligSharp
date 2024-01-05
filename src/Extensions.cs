7ef7abbdb10c731152034f2a3b89596e89b6a644
Kim Ipsen <kim.ipsen@outlook.dk>
2024-01-05 11:10:11 +0100

    Add extension method for fetching all order details in batches

---
+++ src/Extensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using OneOf;
+
+namespace NemligSharp;
+
+public static class Extensions
+{
+    private const int PageSize = 25;
+    public static async Task<OneOf<IList<IOrderResponse>, ErrorResponse>> GetAllOrderDetailsAsync(this INemligClient client, CancellationToken cancellationToken)
+    {
+        if (!client.IsReady) return new List<IOrderResponse>().AsReadOnly();
+
+        List<IOrderResponse> responses = [];
+        int currentPage = 0;
+        int numberOfPages = 0;
+        do
+        {
+            var orderHistory = await client.GetOrderHistoryAsync(currentPage * PageSize, PageSize, cancellationToken).ConfigureAwait(false);
+            if (orderHistory.TryPickT0(out var orderHistoryResponse, out var orderHistoryErrorResponse))
+            {
+                numberOfPages = orderHistoryResponse.NumberOfPages;
+                foreach (var order in orderHistoryResponse.Orders)
+                {
+                    var orderDetails = await client.GetOrderAsync(order.Id, cancellationToken).ConfigureAwait(false);
+                    if (orderDetails.TryPickT0(out var orderDetailsResponse, out var orderDetailsErrorResponse))
+                        responses.Add(orderDetailsResponse);
+                    else
+                        return orderDetailsErrorResponse;
+                }
+            }
+            else
+                return orderHistoryErrorResponse;
+            currentPage++;
+        } while (currentPage < numberOfPages);
+
+        return responses.AsReadOnly();
+    }
+}
